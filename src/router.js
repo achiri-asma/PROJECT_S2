51a24429a66ae2567263ac65144f6a9fc3633716
0wissal <w.herouini@esi-sba.dz>
2024-03-16 22:49:08 +0100

    EditProfile1 code

--- src/router.js
+++ src/router.js
@@ -4,43 +4,20 @@ import SignupDoctor from './pages/SignupDoctor.vue'
 import LoginPage1 from './pages/LoginPage1.vue'
 import LoginPage2 from './pages/LoginPage2.vue'
 import ForgetPassword from './pages/ForgetPassword.vue'
-import DashMed from './pages/DashMed.vue'
 import ResetPassword from './pages/ResetPassword.vue'
+import DashMed from './pages/DashMed.vue'
 
 const routes=[
-    
-    {path:'/signup',
-    component:SignupPage,
-    name: 'SignupPage'
-    },
-    {path:'/login1',
-    component:LoginPage1,
-    name:'LoginPage1'
-    },
-    {path:'/login2',
-    component:LoginPage2,
-    name:'LoginPage2'
-    },
-    {path:'/fg-pass',
-    component:ForgetPassword,
-    name:'ForgetPassword'
-    },
-    {path:'/signup-doc',
-    component:SignupDoctor,
-    name:'SignupDoctor'
-    },
-    {path:'/reset-pass',
-    component:ResetPassword,
-    name:'ResetPassword'
-    },
-    {path:'/',
-    component:DashMed,
-    name:'DashMed'
-    }
+    {path:'/signup',component:SignupPage},
+    {path:'/signup-doc',component:SignupDoctor},
+    {path:'/login1',component:LoginPage1},
+    {path:'/login2',component:LoginPage2},
+    {path:'/fg-pass',component:ForgetPassword},
+    {path:'/reset-pass',component:ResetPassword},
+    {path:'/',component:DashMed}
 ]
 const router = createRouter({
     history:createWebHistory(),
     routes,
-
 })
 export default router;
