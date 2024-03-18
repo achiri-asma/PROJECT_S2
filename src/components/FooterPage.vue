37a88896eac7f039fdd47eea8c7fccc8ea1d5fb7
achiri-asma <a.achiri@esi-sba.dz>
2024-03-18 15:47:44 +0100

    fifth commit

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterPage',   
+}
+</script>
+<style>
+
+footer {
+  margin-top: 1200px; /* Ajoutez une marge supérieure pour séparer le footer du contenu précédent */
+  width: 100%;
+  height: 500px;
+  background-color: #99FEF2;
+}
+</style>
+
+
