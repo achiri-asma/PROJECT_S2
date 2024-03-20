67b01579938f10da9e08a1735ef64e6c415cd8bb
0wissal <w.herouini@esi-sba.dz>
2024-03-20 17:27:31 +0100

    wisss

--- src/pages/DashMed.vue
+++ src/pages/DashMed.vue
@@ -3,9 +3,9 @@
         <aside>
             <img src="../assets/logo.png" alt="logo">
             <ul>
-                <li @click="handleProfile" :class="{'current' : cliked}">Profile</li>
-                <li>History</li>
-                <li>Rating</li>
+                <li @click="handleProfile" :class="{'current' : cliked1}">Profile</li>
+                <li @click="handleHistory" :class="{'current' : cliked2}">History</li>
+                <li @click="handleRating" :class="{'current' : cliked3}">Rating</li>
             </ul>
         </aside>
         <ProfileMed />
@@ -18,13 +18,27 @@ import ProfileMed from './ProfileMed'
 export default {
     data() {
         return {
-            cliked : false
+            cliked1 : true,
+            cliked2 : false,
+            cliked3 : false
         }
     },
     components : { ProfileMed },
     methods : {
         handleProfile() {
-            this.cliked = true
+            this.cliked1 = true,
+            this.cliked2 = false,
+            this.cliked3 = false
+        },
+        handleHistory() {
+            this.cliked1 = false,
+            this.cliked2 = true,
+            this.cliked3 = false
+        },
+        handleRating() {
+            this.cliked1 = false,
+            this.cliked2 = false,
+            this.cliked3 = true
         }
     }
 }
@@ -64,13 +78,9 @@ export default {
         margin-bottom: 10px;
     }
     .dashmed aside li:first-child, .dashuser aside li:first-child{
-        background: url(../assets/user2.png) no-repeat;
+        background: url(../assets/user1.png) no-repeat;
         background-size: 15%;
         background-position: 40px center;
-        background-color: rgb(255, 255, 255, 0.8);
-        border-top-left-radius: 25px;
-        border-bottom-left-radius: 25px;
-        color: #03c6c1;
     }
     .dashmed aside li:first-child + li{
         background: url(../assets/backintime.png) no-repeat;
@@ -83,9 +93,18 @@ export default {
         background-position: 40px center;
     }
     .dashmed aside li.current{
-        background-color: rgb(255, 255, 255, 0.8);
+        background-color: rgba(255, 255, 255, 0.8);
         border-top-left-radius: 25px;
         border-bottom-left-radius: 25px;
         color: #03c6c1;
+    }
+    .dashmed aside li.current:first-child{
+        background-image: url(../assets/user2.png);
+    }
+    .dashmed aside li:first-child + li.current{
+        background-image: url(../assets/backintime2.png);
+    }
+    .dashmed aside li.current:last-child{
+        background-image: url(../assets/rating2.png);
     }
 </style>
